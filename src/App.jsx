da3ffb3c9809d7d77c919a022f75edb10a746c52
Imran-A-K <imran.k.professional@gmail.com>
2023-03-27 16:16:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
